2c13b629e97c61e8bab8db9fa341a0236e1cd2e6
hortuadev <hortuadev@gmail.com>
2026-02-05 10:48:37 +0000

    first commit

---
+++ src/main/java/com/accesodatos/BackendTvCanariaApplication.java
@@ -0,0 +1,13 @@
+package com.accesodatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTvCanariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTvCanariaApplication.class, args);
+	}
+
+}
