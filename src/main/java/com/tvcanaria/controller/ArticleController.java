5056abc00e577a9c18bb4d982f8ad7ec0a71a12c
hortuadev <hortuadev@gmail.com>
2026-02-23 21:09:38 +0000

    feat(controller): add deleteArticle endpoint in ArticleController

--- src/main/java/com/tvcanaria/controller/ArticleController.java
+++ src/main/java/com/tvcanaria/controller/ArticleController.java
@@ -6,6 +6,7 @@ import java.util.Set;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -67,6 +68,12 @@ public class ArticleController {
             @Valid @RequestBody ArticleUpdateRequest request) {
         Article updated = articleService.updateArticle(id, request);
         return ResponseEntity.ok(updated);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteArticle(@PathVariable Integer id) {
+        articleService.deleteArticle(id);
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/test")
