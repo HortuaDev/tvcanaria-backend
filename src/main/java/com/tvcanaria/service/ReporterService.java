09520df99247341a27c9ab3e27c73ae5e0eb198c
oscarrdelgadooy <oscardelyanes@gmail.com>
2026-03-27 10:58:13 +0000

    feat(reporter): created logic for reporterService and created ReporterProfileResponse for controller.

--- src/main/java/com/tvcanaria/service/ReporterService.java
+++ src/main/java/com/tvcanaria/service/ReporterService.java
@@ -3,6 +3,9 @@ package com.tvcanaria.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.tvcanaria.dto.profile.ReporterProfileResponse;
+import com.tvcanaria.entity.User;
+import com.tvcanaria.entity.User.Role;
 import com.tvcanaria.repository.UserRepository;
 
 @Service
@@ -10,4 +13,18 @@ public class ReporterService {
 
     @Autowired
     private UserRepository userRepository;
+
+    public ReporterProfileResponse getReporter(Integer reporterId) {
+
+        User user = userRepository.findById(reporterId)
+                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+
+        Role role = user.getRole();
+
+        if (role != Role.REPORTER || role != Role.ADMIN) {
+            throw new RuntimeException("El usuario no tiene permisos de reportero o administrador");
+        }
+
+        return new ReporterProfileResponse(user.getUsername(), user.getFirstName(), user.getLastName(), user.getCreatedAt());
+    }
 }
