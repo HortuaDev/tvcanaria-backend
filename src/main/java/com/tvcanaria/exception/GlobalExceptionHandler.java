5515410124290c45c7ae714fae3023b43524e20a
Kiara Maldonado <maldonadogarciakiara@gmail.com>
2026-04-02 11:28:56 +0100

    refactor(exception): delete unused imports

--- src/main/java/com/tvcanaria/exception/GlobalExceptionHandler.java
+++ src/main/java/com/tvcanaria/exception/GlobalExceptionHandler.java
@@ -2,7 +2,6 @@ package com.tvcanaria.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,105 +9,103 @@ import org.springframework.web.context.request.WebRequest;
 
 import com.tvcanaria.dto.error.ErrorResponse;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    // --- EXCEPCIONES DE AUTENTICACIÓN ---
+        // --- EXCEPCIONES DE AUTENTICACIÓN ---
 
-    @ExceptionHandler(InvalidCredentialsException.class)
-    public ResponseEntity<ErrorResponse> handleInvalidCredentialsException(InvalidCredentialsException ex,
-            WebRequest request) {
-        ErrorResponse error = new ErrorResponse(
-                HttpStatus.UNAUTHORIZED.value(), // 401: Falta de autenticación válida
-                "Unauthorized",
-                ex.getMessage(),
-                request.getDescription(false).replace("uri=", ""));
-        return new ResponseEntity<>(error, HttpStatus.UNAUTHORIZED);
-    }
+        @ExceptionHandler(InvalidCredentialsException.class)
+        public ResponseEntity<ErrorResponse> handleInvalidCredentialsException(InvalidCredentialsException ex,
+                        WebRequest request) {
+                ErrorResponse error = new ErrorResponse(
+                                HttpStatus.UNAUTHORIZED.value(), // 401: Falta de autenticación válida
+                                "Unauthorized",
+                                ex.getMessage(),
+                                request.getDescription(false).replace("uri=", ""));
+                return new ResponseEntity<>(error, HttpStatus.UNAUTHORIZED);
+        }
 
-    @ExceptionHandler({ AccountDisabledException.class, ForbiddenAccessException.class })
-    public ResponseEntity<ErrorResponse> handleForbiddenExceptions(RuntimeException ex, WebRequest request) {
-        ErrorResponse error = new ErrorResponse(
-                HttpStatus.FORBIDDEN.value(), // 403: Entiende quién eres, pero no te deja pasar
-                "Forbidden",
-                ex.getMessage(),
-                request.getDescription(false).replace("uri=", ""));
-        return new ResponseEntity<>(error, HttpStatus.FORBIDDEN);
-    }
+        @ExceptionHandler({ AccountDisabledException.class, ForbiddenAccessException.class })
+        public ResponseEntity<ErrorResponse> handleForbiddenExceptions(RuntimeException ex, WebRequest request) {
+                ErrorResponse error = new ErrorResponse(
+                                HttpStatus.FORBIDDEN.value(), // 403: Entiende quién eres, pero no te deja pasar
+                                "Forbidden",
+                                ex.getMessage(),
+                                request.getDescription(false).replace("uri=", ""));
+                return new ResponseEntity<>(error, HttpStatus.FORBIDDEN);
+        }
 
-    @ExceptionHandler({ UserAlreadyExistsException.class, DuplicateResourceException.class })
-    public ResponseEntity<ErrorResponse> handleConflictExceptions(RuntimeException ex, WebRequest request) {
-        ErrorResponse error = new ErrorResponse(
-                HttpStatus.CONFLICT.value(), // 409: Conflicto con un recurso existente
-                "Conflict",
-                ex.getMessage(),
-                request.getDescription(false).replace("uri=", ""));
-        return new ResponseEntity<>(error, HttpStatus.CONFLICT);
-    }
+        @ExceptionHandler({ UserAlreadyExistsException.class, DuplicateResourceException.class })
+        public ResponseEntity<ErrorResponse> handleConflictExceptions(RuntimeException ex, WebRequest request) {
+                ErrorResponse error = new ErrorResponse(
+                                HttpStatus.CONFLICT.value(), // 409: Conflicto con un recurso existente
+                                "Conflict",
+                                ex.getMessage(),
+                                request.getDescription(false).replace("uri=", ""));
+                return new ResponseEntity<>(error, HttpStatus.CONFLICT);
+        }
 
-    // --- EXCEPCIONES GENERALES DEL NEGOCIO ---
+        // --- EXCEPCIONES GENERALES DEL NEGOCIO ---
 
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException ex,
-            WebRequest request) {
-        ErrorResponse error = new ErrorResponse(
-                HttpStatus.NOT_FOUND.value(), // 404
-                "Not Found",
-                ex.getMessage(),
-                request.getDescription(false).replace("uri=", ""));
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
+        @ExceptionHandler(ResourceNotFoundException.class)
+        public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException ex,
+                        WebRequest request) {
+                ErrorResponse error = new ErrorResponse(
+                                HttpStatus.NOT_FOUND.value(), // 404
+                                "Not Found",
+                                ex.getMessage(),
+                                request.getDescription(false).replace("uri=", ""));
+                return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+        }
 
-    @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<ErrorResponse> handleBadRequestException(BadRequestException ex, WebRequest request) {
-        ErrorResponse error = new ErrorResponse(
-                HttpStatus.BAD_REQUEST.value(), // 400
-                "Bad Request",
-                ex.getMessage(),
-                request.getDescription(false).replace("uri=", ""));
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
-    }
+        @ExceptionHandler(BadRequestException.class)
+        public ResponseEntity<ErrorResponse> handleBadRequestException(BadRequestException ex, WebRequest request) {
+                ErrorResponse error = new ErrorResponse(
+                                HttpStatus.BAD_REQUEST.value(), // 400
+                                "Bad Request",
+                                ex.getMessage(),
+                                request.getDescription(false).replace("uri=", ""));
+                return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+        }
 
-    // --- MANEJO DE ERRORES DE VALIDACIÓN (@Valid) ---
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex,
-            WebRequest request) {
-        String errorMessage = ex.getBindingResult().getFieldErrors().stream()
-                .map(error -> error.getField() + ": " + error.getDefaultMessage())
-                .collect(Collectors.joining(", "));
+        // --- MANEJO DE ERRORES DE VALIDACIÓN (@Valid) ---
+        @ExceptionHandler(MethodArgumentNotValidException.class)
+        public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex,
+                        WebRequest request) {
+                String errorMessage = ex.getBindingResult().getFieldErrors().stream()
+                                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                                .collect(Collectors.joining(", "));
 
-        ErrorResponse errorResponse = new ErrorResponse(
-                HttpStatus.BAD_REQUEST.value(), // 400
-                "Validation Error",
-                errorMessage,
-                request.getDescription(false).replace("uri=", ""));
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
-    }
+                ErrorResponse errorResponse = new ErrorResponse(
+                                HttpStatus.BAD_REQUEST.value(), // 400
+                                "Validation Error",
+                                errorMessage,
+                                request.getDescription(false).replace("uri=", ""));
+                return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+        }
 
-    // --- FALLBACK GENÉRICO ---
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleGlobalException(Exception ex, WebRequest request) {
-        ErrorResponse error = new ErrorResponse(
-                HttpStatus.INTERNAL_SERVER_ERROR.value(),
-                "Internal Server Error",
-                "Ha ocurrido un error inesperado",
-                request.getDescription(false).replace("uri=", ""));
-        ex.printStackTrace();
-        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+        // --- FALLBACK GENÉRICO ---
+        @ExceptionHandler(Exception.class)
+        public ResponseEntity<ErrorResponse> handleGlobalException(Exception ex, WebRequest request) {
+                ErrorResponse error = new ErrorResponse(
+                                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                                "Internal Server Error",
+                                "Ha ocurrido un error inesperado",
+                                request.getDescription(false).replace("uri=", ""));
+                ex.printStackTrace();
+                return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
 
-    @ExceptionHandler(ExternalServiceException.class)
-    public ResponseEntity<ErrorResponse> handleExternalServiceException(ExternalServiceException ex,
-            WebRequest request) {
-        ErrorResponse error = new ErrorResponse(
-                HttpStatus.BAD_GATEWAY.value(), // 502: Problema comunicándose con un servicio externo
-                "Bad Gateway",
-                ex.getMessage(),
-                request.getDescription(false).replace("uri=", ""));
-        return new ResponseEntity<>(error, HttpStatus.BAD_GATEWAY);
-    }
+        @ExceptionHandler(ExternalServiceException.class)
+        public ResponseEntity<ErrorResponse> handleExternalServiceException(ExternalServiceException ex,
+                        WebRequest request) {
+                ErrorResponse error = new ErrorResponse(
+                                HttpStatus.BAD_GATEWAY.value(), // 502: Problema comunicándose con un servicio externo
+                                "Bad Gateway",
+                                ex.getMessage(),
+                                request.getDescription(false).replace("uri=", ""));
+                return new ResponseEntity<>(error, HttpStatus.BAD_GATEWAY);
+        }
 }
